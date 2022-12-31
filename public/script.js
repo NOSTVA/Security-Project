51497c8bebf116733d31165bced21db76b0c7864
NOSTVA <nostva9000@gmail.com>
2022-12-31 03:22:56 +0200

    as

--- public/script.js
+++ public/script.js
@@ -1,58 +1,61 @@
-chars = [ ' ', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
-'M', 'N', 'O', 'P', 'Q', 'R',  'S', 'T', 'U', 'V', 'W', 'X',
-'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
-'m', 'n', 'o', 'p', 'q', 'r',  's', 't', 'u', 'v', 'w', 'x',
-'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' ]
+chars = [' ', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
+    'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X',
+    'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+    'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x',
+    'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'
+]
 
 
-window.onload = async function() {
+window.onload = async function () {
 
-const publicKeyField = document.getElementById('public-key')
-const messageRSAField = document.getElementById('RSA-message-Field')
-const btnRSA = document.getElementById('send-RSA-btn')
+    const publicKeyField = document.getElementById('public-key')
+    const messageRSAField = document.getElementById('RSA-message-Field')
+    const btnRSA = document.getElementById('send-RSA-btn')
 
-function encrypt(msg, e, n)  {
-    return BigNumber(msg).exponentiatedBy(e).modulo(n).toNumber();
-}
-
-
-// GET RSA API
-const publicRSA_res = await fetch("/RSA")
-const publicRSA_data = await publicRSA_res.json()
-const e = publicRSA_data.E
-const n = publicRSA_data.n
-
-publicKeyField.innerText = `(${e}, ${n})`
-
-
-// POST encrypted message
-btnRSA.addEventListener("click", async () => {
-
-    msg = messageRSAField.value
-    var cipherList = []
-    
-    if (msg != "")
-    {
-        for (i=0; i<msg.length; i++) {
-            char = encrypt(chars.indexOf(msg[i]), e, n)
-            cipherList.push(char)
-        }
-        const options = {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({cipherList}),
-        }
-        const res = await fetch("/RSA", options)
-        const resData = await res.json()
-        console.log(resData)
-
+    function encrypt(msg, e, n) {
+        return BigNumber(msg).exponentiatedBy(e).modulo(n).toNumber();
     }
 
 
-    
+    // GET RSA API
+    const publicRSA_res = await fetch("/RSA")
+    const publicRSA_data = await publicRSA_res.json()
+    const e = publicRSA_data.E
+    const n = publicRSA_data.n
+
+    publicKeyField.innerText = `(${e}, ${n})`
 
 
-})
+    // POST encrypted message
+    btnRSA.addEventListener("click", async () => {
+
+        msg = messageRSAField.value
+        var cipherList = []
+
+        if (msg != "") {
+            for (i = 0; i < msg.length; i++) {
+                char = encrypt(chars.indexOf(msg[i]), e, n)
+                cipherList.push(char)
+            }
+            const options = {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                    cipherList
+                }),
+            }
+            const res = await fetch("/RSA", options)
+            const resData = await res.json()
+            console.log(resData)
+        }
+
+
+
+
+
+    })
 
 
 
--- routes/rsaRouter.js
+++ routes/rsaRouter.js
@@ -1,19 +1,24 @@
-chars = [ ' ', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
-'M', 'N', 'O', 'P', 'Q', 'R',  'S', 'T', 'U', 'V', 'W', 'X',
-'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
-'m', 'n', 'o', 'p', 'q', 'r',  's', 't', 'u', 'v', 'w', 'x',
-'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' ]
+chars = [' ', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
+    'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X',
+    'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+    'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x',
+    'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'
+]
 
 const express = require('express')
 const router = express.Router()
-const {e, modular_inverse, decrypt} = require('../controllers/RSA')
+const {
+    e,
+    modular_inverse,
+    decrypt
+} = require('../controllers/RSA')
 
-p = 13
+p = 11
 q = 7
-n = p*q
-phi = (p-1)*(q-1)
+n = p * q
+phi = (p - 1) * (q - 1)
 E = e(phi)
-d= modular_inverse(E,phi)
+d = modular_inverse(E, phi)
 
 
 
@@ -22,19 +27,24 @@ d= modular_inverse(E,phi)
 router.post('/', async (req, res) => {
     cipherList = req.body.cipherList
     msgList = []
-    for (i=0; i<cipherList.length; i++) {
+    for (i = 0; i < cipherList.length; i++) {
         char = decrypt(cipherList[i], d, n)
         msgList.push(chars[char])
     }
 
     console.log("(RSA) Encrypted message: ", cipherList.join(''))
     console.log("(RSA) Decrypted message: ", msgList.join(''))
-    res.json({message: '(RSA) Message received!'})
+    res.json({
+        message: '(RSA) Message received!'
+    })
 })
 
 
-router.get('/', (req,res) => {
-    res.json({E: E, n: n})
+router.get('/', (req, res) => {
+    res.json({
+        E: E,
+        n: n
+    })
 })
 
 module.exports = router
